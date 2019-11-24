be238563ae23765bd6f9d2d7734c5009a66bdfce
mrclag <mattrclagett@gmail.com>
2019-11-24 14:51:27 -0800

    memoized cell component to only rerender on changes - fixed rendering bug when dropping

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { StyledCell } from './styles/Cell.styles.js';
-import { TETROMINOS } from '../tetrominos.js';
+import { StyledCell } from './styles/Cell.styles';
+import { TETROMINOS } from '../tetrominos';
 
-const Cell = ({ type }) => {
-  return <StyledCell type={type} color={TETROMINOS[type].color} />;
-};
+// React.memo makes sure we only re-render the changed cells
+const Cell = ({ type }) => (
+  <StyledCell type={type} color={TETROMINOS[type].color}>
+    {console.log('rerender cell')}
+  </StyledCell>
+);
 
-export default Cell;
+export default React.memo(Cell);
