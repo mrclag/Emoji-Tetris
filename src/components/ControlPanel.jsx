0816b14db527861cf84b58d13dc6608e6ee046ba
mrclag <mattrclagett@gmail.com>
2019-11-25 02:23:16 -0800

    added https redirect and enabled service worker

--- src/components/ControlPanel.jsx
+++ src/components/ControlPanel.jsx
@@ -5,9 +5,7 @@ import down from '../img/down.png';
 import rotate from '../img/rotate-right.png';
 
 import { ControlButton, ControlWrapper } from './styles/ControlPanel.styles';
-import { useInterval } from '../hooks/useInterval';
 
-let interval = null;
 const ControlPanel = ({
   movePlayer,
   dropPlayer,
@@ -18,23 +16,12 @@ const ControlPanel = ({
 }) => {
   return (
     <ControlWrapper>
-      <ControlButton
-        src={left}
-        alt="left"
-        onTouchStart={() => {
-          interval = setInterval(() => movePlayer(-1), 100);
-        }}
-        onTouchEnd={clearInterval(interval)}
-      />
+      <ControlButton src={left} alt="left" onClick={() => movePlayer(-1)} />
       <ControlButton
         src={down}
         alt="down"
-        onTouchStart={() => {
-          console.log(interval);
-          interval = setInterval(dropPlayer(), 200);
-        }}
-        onTouchEnd={() => {
-          clearInterval(interval);
+        onClick={() => {
+          dropPlayer();
           setDropTime(1000 / (level + 1));
         }}
       />
