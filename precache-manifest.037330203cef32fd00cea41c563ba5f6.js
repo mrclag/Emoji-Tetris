b26d1d97802351f8a41252216743872b7bb5ddbf
mrclag <mattrclagett@gmail.com>
2019-12-04 15:08:00 -0800

    Updates

---
+++ precache-manifest.037330203cef32fd00cea41c563ba5f6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72d81ea7d463c76002106b4316416e1b",
+    "url": "/Tetris/index.html"
+  },
+  {
+    "revision": "e7271a69823e229174b7",
+    "url": "/Tetris/static/css/main.e9aa6a1f.chunk.css"
+  },
+  {
+    "revision": "b514de768f657527a48b",
+    "url": "/Tetris/static/js/2.3f620194.chunk.js"
+  },
+  {
+    "revision": "e7271a69823e229174b7",
+    "url": "/Tetris/static/js/main.6a425740.chunk.js"
+  },
+  {
+    "revision": "25e09a0b4966e209902e",
+    "url": "/Tetris/static/js/runtime-main.e5f8c7b0.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/Tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/Tetris/static/media/bg.2810fe6b.png"
+  },
+  {
+    "revision": "cf9ed1dd469328409a1389f2150f4a58",
+    "url": "/Tetris/static/media/rotate-right.cf9ed1dd.png"
+  }
+]);
