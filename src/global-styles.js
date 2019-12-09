61a5b495b975eae4a1301343d21f39f6142e14f5
mrclag <mattrclagett@gmail.com>
2019-12-09 01:35:07 -0800

    added high scores functionality with Firebase, modal at Game Over for name input

---
+++ src/global-styles.js
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+
+  @font-face {
+    font-family: 'Orbitron';
+    src: url('./font/Orbitron-Regular.ttf');
+    font-weight: normal;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'Orbitron';
+    src: url('./font/Orbitron-Bold.ttf');
+    font-weight: bold;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Pixel';
+    src: url('font/Pixel-LCD-7.woff') format('woff');
+  }
+
+  body {
+    margin: 0;
+  }
+`;
+
+export default GlobalStyle;
