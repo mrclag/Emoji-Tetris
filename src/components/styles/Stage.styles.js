ad5e7642c0c00e603de7fee0d73e76b889eb72ec
mrclag <mattrclagett@gmail.com>
2020-03-05 15:00:29 -0800

    changed control panel icons for mobile to better suit theme

--- src/components/styles/Stage.styles.js
+++ src/components/styles/Stage.styles.js
@@ -4,13 +4,13 @@ export const StyledStage = styled.div`
   display: grid;
   grid-template-rows: repeat(
     ${props => props.height},
-    calc(450px / ${props => props.width})
+    calc(420px / ${props => props.width})
   );
   grid-template-columns: repeat(${props => props.width}, 1fr);
   grid-gap: 1px;
   border: 3px solid #333;
   width: 100%;
-  max-width: 450px;
+  max-width: 420px;
   background: #f7f7f7;
 
   @media screen and (max-width: 800px) {
